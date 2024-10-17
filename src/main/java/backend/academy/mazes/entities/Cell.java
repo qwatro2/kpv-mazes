e81c876ecf736d9f09e09aba7ff18257272a3fa7
qwatro2 <kv22828@gmail.com>
2024-10-18 01:07:09 +0300

    fix: remove unused annotation

--- src/main/java/backend/academy/mazes/entities/Cell.java
+++ src/main/java/backend/academy/mazes/entities/Cell.java
@@ -1,7 +1,5 @@
 package backend.academy.mazes.entities;
 
-import lombok.Getter;
-
 public record Cell(int row, int col) {
     public Coordinate getCoordinate() {
         return new Coordinate(this.row, this.col);
