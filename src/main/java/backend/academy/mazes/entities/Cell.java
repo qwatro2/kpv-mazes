352ae95ca9c18aedf90022a3d11b79e3839c189a
qwatro2 <kv22828@gmail.com>
2024-10-17 23:55:18 +0300

    fix: remove unused things from Cell.java

--- src/main/java/backend/academy/mazes/entities/Cell.java
+++ src/main/java/backend/academy/mazes/entities/Cell.java
@@ -1,30 +1,13 @@
 package backend.academy.mazes.entities;
 
-import backend.academy.mazes.commons.Direction;
 import lombok.Getter;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter public class Cell {
     private final int row;
     private final int col;
-    private boolean rightWall;
-    private boolean bottomWall;
 
     public Cell(int row, int col) {
         this.row = row;
         this.col = col;
-        this.rightWall = false;
-        this.bottomWall = false;
-    }
-
-    public Cell setWallsAndPassages(int height, int width) {
-       if (this.row == height - 1) {
-           this.bottomWall = true;
-       }
-       if (this.col == width - 1) {
-           this.rightWall = true;
-       }
-       return this;
     }
 }
