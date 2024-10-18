6351dc822c2d091ec677478cf8027fb1f32b9dbc
qwatro2 <kv22828@gmail.com>
2024-10-18 16:56:48 +0300

    fit: implement Kruskal MazeGenerator

---
+++ src/main/java/backend/academy/mazes/generators/KruskalMazeGenerator.java
@@ -0,0 +1,96 @@
+package backend.academy.mazes.generators;
+
+import backend.academy.mazes.commons.Randomizable;
+import backend.academy.mazes.entities.Cell;
+import backend.academy.mazes.entities.Coordinate;
+import backend.academy.mazes.entities.Edge;
+import backend.academy.mazes.entities.Maze;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+public class KruskalMazeGenerator extends AbstractMazeGenerator
+    implements MazeGenerator, Randomizable<KruskalMazeGenerator> {
+    private Random random;
+
+    @Override
+    public Maze generate(int height, int width) {
+        Cell[][] cells = makeCells(height, width);
+        List<Edge> startEdges = getBaseEdges(height, width);
+        List<Edge> edges = kruskal(height, width, startEdges);
+        boolean[][] grid = getGridByEdges(height, width, edges);
+        return new Maze(height, width, cells, grid);
+    }
+
+    private List<Edge> kruskal(int height, int width, List<Edge> startEdges) {
+        int numberOfCells = height * width;
+        List<Edge> result = new ArrayList<>();
+
+        List<Integer> parent = initializeEmptyList(numberOfCells);
+        List<Integer> rank = initializeEmptyList(numberOfCells);
+        for (int i = 0; i < numberOfCells; ++i) {
+            makeSet(parent, rank, i);
+        }
+
+        while (!startEdges.isEmpty()) {
+            Edge randomEdge = popRandomEdge(startEdges);
+            int fromCoordinate = coordinateToIndex(randomEdge.from(), width);
+            int toCoordinate = coordinateToIndex(randomEdge.to(), width);
+            if (findSet(parent, fromCoordinate) != findSet(parent, toCoordinate)) {
+                result.add(randomEdge);
+                unionSets(parent, rank, fromCoordinate, toCoordinate);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Integer> initializeEmptyList(int size) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < size; ++i) {
+            result.add(0);
+        }
+        return result;
+    }
+
+    private Edge popRandomEdge(List<Edge> edges) {
+        int index = this.random.nextInt(0, edges.size());
+        return edges.remove(index);
+    }
+
+    private void makeSet(List<Integer> parent, List<Integer> rank, int vertexIndex) {
+        parent.set(vertexIndex, vertexIndex);
+        rank.set(vertexIndex, 0);
+    }
+
+    private int findSet(List<Integer> parent, int vertexIndex) {
+        if (vertexIndex == parent.get(vertexIndex)) {
+            return vertexIndex;
+        }
+        parent.set(vertexIndex, findSet(parent, parent.get(vertexIndex)));
+        return parent.get(vertexIndex);
+    }
+
+    private void unionSets(List<Integer> parent, List<Integer> rank, int setA, int setB) {
+        setA = findSet(parent, setA);
+        setB = findSet(parent, setB);
+        if (setA != setB) {
+            if (rank.get(setA) < rank.get(setB)) {
+                int tmp = setA;
+                setA = setB;
+                setB = tmp;
+            }
+            parent.set(setB, setA);
+            if (Objects.equals(rank.get(setA), rank.get(setB))) {
+                rank.set(setA, 1+ rank.get(setA));
+            }
+        }
+    }
+
+    @Override
+    public KruskalMazeGenerator setRandom(Random random) {
+        this.random = random;
+        return this;
+    }
+}
