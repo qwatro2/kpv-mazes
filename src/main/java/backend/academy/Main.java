be5656ed226d5de843575977b7783515d7ffb9ad
qwatro2 <kv22828@gmail.com>
2024-10-20 04:41:40 +0300

    fit: version 0.0.1 TODO: 1. add different cell's content 2. checkout code and refactor

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,21 @@
 package backend.academy;
 
+import backend.academy.mazes.app.App;
+import backend.academy.mazes.app.MazeApp;
+import backend.academy.mazes.waiters.ConsoleWaiter;
+import backend.academy.mazes.readers.ConsoleReader;
+import backend.academy.mazes.writers.ConsoleWriter;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
+
     public static void main(String[] args) {
+        App mazeApp = new MazeApp(
+            new ConsoleReader().setPrintStream(System.out).setInputStream(System.in),
+            new ConsoleWriter().setPrintStream(System.out),
+            new ConsoleWaiter().setPrintStream(System.out).setInputStream(System.in)
+        );
+        mazeApp.run();
     }
 }
