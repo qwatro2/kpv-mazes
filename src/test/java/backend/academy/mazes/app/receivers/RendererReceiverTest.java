ba8d85764d5fdfe46cf164bb412793c4c2f446f8
qwatro2 <kv22828@gmail.com>
2024-10-21 18:58:22 +0300

    test: add RendererReceiver test

--- src/test/java/backend/academy/mazes/app/receivers/RendererReceiverTest.java
+++ src/test/java/backend/academy/mazes/app/receivers/RendererReceiverTest.java
@@ -2,29 +2,29 @@ package backend.academy.mazes.app.receivers;
 
 import backend.academy.mazes.app.MazeAppState;
 import backend.academy.mazes.commons.EnumRandomPicker;
-import backend.academy.mazes.generators.PrimMazeGenerator;
 import backend.academy.mazes.readers.Reader;
-import backend.academy.mazes.types.GeneratorType;
+import backend.academy.mazes.types.RendererType;
 import backend.academy.mazes.writers.Writer;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class GeneratorReceiverTest {
+class RendererReceiverTest {
 
     @Test
     void receive() {
         Reader reader = mock(Reader.class);
-        when(reader.readGeneratorType()).thenReturn(GeneratorType.PRIM);
+        when(reader.readRendererType()).thenReturn(RendererType.COLORFUL);
 
         Writer writer = mock(Writer.class);
         EnumRandomPicker picker = mock(EnumRandomPicker.class);
 
         MazeAppState state = new MazeAppState();
-        Receiver receiver = new GeneratorReceiver(reader, writer, picker);
+
+        Receiver receiver = new RendererReceiver(reader, writer, picker);
         receiver.receive(state);
 
-        assertInstanceOf(PrimMazeGenerator.class, state.generator());
+        assertTrue(state.renderer().getLegend().contains("\uD83D\uDFE9"));
     }
 }
