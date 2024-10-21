4b4fd43a0c73df98ed883d2b97745f246003cf47
qwatro2 <kv22828@gmail.com>
2024-10-21 19:18:07 +0300

    test: add SolverReceiver test

---
+++ src/test/java/backend/academy/mazes/app/receivers/SolverReceiverTest.java
@@ -0,0 +1,32 @@
+package backend.academy.mazes.app.receivers;
+
+import backend.academy.mazes.app.MazeAppState;
+import backend.academy.mazes.commons.EnumRandomPicker;
+import backend.academy.mazes.generators.PrimMazeGenerator;
+import backend.academy.mazes.readers.Reader;
+import backend.academy.mazes.solvers.BfsMazeSolver;
+import backend.academy.mazes.types.GeneratorType;
+import backend.academy.mazes.types.SolverType;
+import backend.academy.mazes.writers.Writer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class SolverReceiverTest {
+
+    @Test
+    void receive() {
+        Reader reader = mock(Reader.class);
+        when(reader.readSolverType()).thenReturn(SolverType.BFS);
+
+        Writer writer = mock(Writer.class);
+        EnumRandomPicker picker = mock(EnumRandomPicker.class);
+
+        MazeAppState state = new MazeAppState();
+        Receiver receiver = new SolverReceiver(reader, writer, picker);
+        receiver.receive(state);
+
+        assertInstanceOf(BfsMazeSolver.class, state.solver());
+    }
+}
