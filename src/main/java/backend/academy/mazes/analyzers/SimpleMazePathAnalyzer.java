8e4ea88b0170613d4e8b15918351227891905c6e
qwatro2 <kv22828@gmail.com>
2024-10-22 02:24:44 +0300

    style: switch without default branch

--- src/main/java/backend/academy/mazes/analyzers/SimpleMazePathAnalyzer.java
+++ src/main/java/backend/academy/mazes/analyzers/SimpleMazePathAnalyzer.java
@@ -22,6 +22,7 @@ public class SimpleMazePathAnalyzer implements MazePathAnalyzer {
                     pathStatistics.incrementBadPassages();
                     break;
                 case EMPTY:
+                    break;
             }
         }
 
