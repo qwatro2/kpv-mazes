7b819bb63dc2dcebd80231d676d05d63fe88ac9c
qwatro2 <kv22828@gmail.com>
2024-10-21 19:00:25 +0300

    test: add SizeReceiver test

---
+++ src/test/java/backend/academy/mazes/app/receivers/SizeReceiverTest.java
@@ -0,0 +1,28 @@
+package backend.academy.mazes.app.receivers;
+
+import backend.academy.mazes.app.MazeAppState;
+import backend.academy.mazes.readers.Reader;
+import backend.academy.mazes.writers.Writer;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class SizeReceiverTest {
+
+    @Test
+    void receive() {
+        Reader reader = mock(Reader.class);
+        when(reader.readHeight()).thenReturn(10);
+        when(reader.readWidth()).thenReturn(30);
+
+        Writer writer = mock(Writer.class);
+
+        MazeAppState state = new MazeAppState();
+        Receiver receiver = new SizeReceiver(reader, writer);
+        receiver.receive(state);
+
+        assertEquals(10, state.height());
+        assertEquals(30, state.width());
+    }
+}
