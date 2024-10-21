ec241e3bacf7dc9ae4073e44271eb2c79f0e7d6c
qwatro2 <kv22828@gmail.com>
2024-10-21 16:19:33 +0300

    fit: add CellType to Cell

--- src/main/java/backend/academy/mazes/entities/Cell.java
+++ src/main/java/backend/academy/mazes/entities/Cell.java
@@ -1,6 +1,19 @@
 package backend.academy.mazes.entities;
 
-public record Cell(int row, int col) {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter public class Cell {
+    private final int row;
+    private final int col;
+    @Setter private CellType cellType;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.cellType = CellType.EMPTY;
+    }
+
     public Coordinate getCoordinate() {
         return new Coordinate(this.row, this.col);
     }
