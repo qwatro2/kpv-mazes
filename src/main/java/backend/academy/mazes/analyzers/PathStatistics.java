693680f19931d4827bb2d7c399d0a8c73d050b27
qwatro2 <kv22828@gmail.com>
2024-10-21 16:18:48 +0300

    fit: add PathStatistics interface and its implementation SimplePathStatistics

---
+++ src/main/java/backend/academy/mazes/analyzers/PathStatistics.java
@@ -0,0 +1,5 @@
+package backend.academy.mazes.analyzers;
+
+public interface PathStatistics {
+    String constructString();
+}
---
+++ src/main/java/backend/academy/mazes/analyzers/SimplePathStatistics.java
@@ -0,0 +1,29 @@
+package backend.academy.mazes.analyzers;
+
+public class SimplePathStatistics implements PathStatistics {
+    private int numberOfGoodPassages;
+    private int numberOfBadPassages;
+
+    public SimplePathStatistics() {
+        this(0, 0);
+    }
+
+    private SimplePathStatistics(int numberOfGoodPassages, int numberOfBadPassages) {
+        this.numberOfGoodPassages = numberOfGoodPassages;
+        this.numberOfBadPassages = numberOfBadPassages;
+    }
+
+    public void incrementGoodPassages() {
+        ++this.numberOfGoodPassages;
+    }
+
+    public void incrementBadPassages() {
+        ++this.numberOfBadPassages;
+    }
+
+    @Override
+    public String constructString() {
+        return "Number of good passages: " + this.numberOfGoodPassages + "\n" +
+            "Number of bad passages: " + this.numberOfBadPassages;
+    }
+}
