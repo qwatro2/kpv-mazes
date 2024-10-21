f7f9bbc73b83f9a55740ca49f76830b367972f6d
qwatro2 <kv22828@gmail.com>
2024-10-21 16:18:20 +0300

    fit: add MazeFiller interface and its implementation RandomMazeFiller

---
+++ src/main/java/backend/academy/mazes/fillers/MazeFiller.java
@@ -0,0 +1,7 @@
+package backend.academy.mazes.fillers;
+
+import backend.academy.mazes.entities.Maze;
+
+public interface MazeFiller {
+    void fill(Maze maze);
+}
---
+++ src/main/java/backend/academy/mazes/fillers/RandomMazeFiller.java
@@ -0,0 +1,31 @@
+package backend.academy.mazes.fillers;
+
+import backend.academy.mazes.commons.Randomizable;
+import backend.academy.mazes.entities.CellType;
+import backend.academy.mazes.entities.Maze;
+import java.util.Random;
+
+public class RandomMazeFiller implements MazeFiller, Randomizable<RandomMazeFiller> {
+    private Random random;
+
+    @Override
+    public void fill(Maze maze) {
+        for (int row = 0; row < maze.height(); ++row) {
+            for (int col = 0; col < maze.width(); ++col) {
+                maze.cells()[row][col].cellType(getRandomCellType());
+            }
+        }
+    }
+
+    private CellType getRandomCellType() {
+        CellType[] cellTypes = CellType.values();
+        int cellTypeIndex = random.nextInt(0, cellTypes.length);
+        return cellTypes[cellTypeIndex];
+    }
+
+    @Override
+    public RandomMazeFiller setRandom(Random random) {
+        this.random = random;
+        return this;
+    }
+}
